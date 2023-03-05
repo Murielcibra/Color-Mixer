221141ae5ee80e83d0c5cd1102d9e94ce56e6a89
Muriel Cishek <muriel_cibra@hotmail.com>
2023-03-05 21:22:41 +0100

    Random Color Btn added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict";
+const api = "https://dummy-apis.netlify.app/api/color";
 const redRangeInput = document.getElementById("red");
 const greenRangeInput = document.getElementById("green");
 const blueRangeInput = document.getElementById("blue");
 const hexP = document.getElementById("hex-value");
-
+const btnEl = document.getElementById("btnRandom");
 function colors() {
   const red = parseInt(redRangeInput.value);
   const green = parseInt(greenRangeInput.value);
@@ -22,3 +23,21 @@ greenRangeInput.addEventListener("input", colors);
 blueRangeInput.addEventListener("input", colors);
 
 colors();
+
+function randomColors() {
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      // extracts the r, g, and b values
+      const r = data.rgb.r;
+      const g = data.rgb.g;
+      const b = data.rgb.b;
+      // Set the range input values to the fetched RGB values
+      redRangeInput.value = r;
+      greenRangeInput.value = g;
+      blueRangeInput.value = b;
+      // Call the colors function to update the background color and hex value
+      colors();
+    });
+}
+btnEl.addEventListener("click", randomColors);
